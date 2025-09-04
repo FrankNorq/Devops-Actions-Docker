f988bf47ce7c71beb3a73f4df00a40e361c4c1a7
Frank <Frankuik@hotmail.com>
2025-09-04 15:16:17 +0200

    please

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,9 +19,6 @@ function App() {
       <div>
         <p>bajskorv</p>
         <p>bajskorv</p>
-        <p>bajskorv</p>
-        <p>bajskorv</p>
-        <p>bajskorv</p>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
