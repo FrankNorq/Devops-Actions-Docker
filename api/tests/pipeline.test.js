b5767725f8bf1778ca7c98c6da7ffc6f4bd43782
Frank <Frankuik@hotmail.com>
2025-09-24 13:47:35 +0200

    works now

--- api/tests/pipeline.test.js
+++ api/tests/pipeline.test.js
@@ -2,7 +2,7 @@ describe("Pipeline Test", () => {
   it("should confirm that everyything is working", () => {
     const a = 10;
     const b = 5;
-    let c = 10;
-    expect(a + b).toBe(c);
+    let x = a + b;
+    expect(a + b).toBe(x);
   });
 });
