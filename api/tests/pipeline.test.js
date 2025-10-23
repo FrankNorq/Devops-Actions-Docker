e4a55cfe998be7e00339c17a6c0fd36fbc39cd81
Frank <Frankuik@hotmail.com>
2025-10-23 16:07:14 +0200

    error

--- api/tests/pipeline.test.js
+++ api/tests/pipeline.test.js
@@ -2,7 +2,7 @@ describe("Pipeline Test", () => {
   it("should confirm that everyything is working", () => {
     const a = 10;
     const b = 5;
-    let x = a + b;
+    let x = a + b + b;
     expect(a + b).toBe(y);
   });
 });
